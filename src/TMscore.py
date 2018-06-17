a6d0112c9dc1d79c7baaa768bc31662b33812bc4
h3nnn4n <uber.renan@gmail.com>
2018-06-17 14:17:40 -0300

    Now processes gtd-ts, gdt-ha, rmsd and tmscore

--- src/TMscore.py
+++ src/TMscore.py
@@ -11,6 +11,11 @@ class TMscore():
         else:
             raise Exception("%s was not found" % path)
 
+        self.rmsd = None
+        self.gdt_ts = None
+        self.gdt_ha = None
+        self.tm_score = None
+
     def __call__(self, prot_a, prot_b):
         if os.path.isfile(prot_a) and os.path.isfile(prot_b):
             out = subprocess.check_output([self.path, prot_a, prot_b])
@@ -18,11 +23,24 @@ class TMscore():
             for d in data:
                 x = re.sub("\s\s+", " ", d).split(' ')
                 if x[0] == "TM-score" and x[1] == "=":
-                    return float(x[2])
+                    self.tm_score = float(x[2])
+                elif x[0] == "GDT-TS-score=":
+                    self.gdt_ts = float(x[1])
+                elif x[0] == "GDT-HA-score=":
+                    self.gdt_ha = float(x[1])
+                elif x[0] == "RMSD":
+                    self.rmsd = float(x[5])
         else:
             raise Exception("Check that %s and %s exists" % (prot_a, prot_b))
 
+    def info(self):
+        print(self.rmsd)
+        print(self.tm_score)
+        print(self.gdt_ts)
+        print(self.gdt_ha)
+
 
 if __name__ == "__main__":
-    tmscore = TMscore("/home/h3nnn4n/TMscore.py/src/TMscore")
-    print(tmscore("/home/h3nnn4n/1crn.pdb", "/home/h3nnn4n/pspp.jl/src/simulated_annealing.py/best.pdb"))
+    tmscore = TMscore("./TMscore")
+    tmscore("./1crn.pdb", "./best.pdb")
+    tmscore.info()
